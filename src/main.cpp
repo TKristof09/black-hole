1ae14b789c57fd4ed03bd290f470c62bfbe05046
Kristof Toth <toth.kristof111@gmail.com>
2025-08-21 15:23:54 +0200

    Implement camera orbit rotation

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,13 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <imgui.h>
 
+constexpr uint32_t WIDTH  = 1280;
+constexpr uint32_t HEIGHT = 720;
+
+
 int main()
 {
-    Application app(800, 600, 60);
+    Application app(WIDTH, HEIGHT, 60);
     PipelineCreateInfo ci = {};
     ci.type               = PipelineType::COMPUTE;
     ci.stages             = VK_SHADER_STAGE_COMPUTE_BIT;
@@ -17,41 +21,42 @@ int main()
 
     Image background = Image::CubemapFromFile("resources/milky_way_cubemap");
 
-    glm::vec3 rot{0.0};
-    float distance = 30.f;
+    float distance   = 30.f;
+    float orbitAngle = 0.f;
+
+    int32_t simNumSteps = 1e4;
+    float simStepSize   = 1e-2;
 
     Shader& shader = p.GetShader(0);
 
     app.EnqueueRenderCommand([&](CommandBuffer& cb, Image& frameBuffer, uint32_t frameIndex, float dt)
                              {
-             ImGui::DragFloat3("Rotation", glm::value_ptr(rot), 1.f, -180.f, 180.f);
+
+             ImGui::PushItemWidth(ImGui::GetFontSize() * 6);
+             ImGui::SeparatorText("Camera configuration");
+             ImGui::DragFloat("Orbit angle", &orbitAngle, 1.f, -180.f, 180.f);
              ImGui::DragFloat("Camera distance", &distance, 0.5f, 1.f, 100.f);
+             ImGui::SeparatorText("Simulation configuration");
+             ImGui::DragInt("Number of simulation step", &simNumSteps, 100, 0, 1e6);
+             ImGui::DragFloat("Simulation step size", &simStepSize, 0.001f, 1e-6, 1.f);
+             ImGui::PopItemWidth();
 
-             float pitch = glm::radians(rot.x);
-             float yaw   = glm::radians(rot.y);
-             float roll  = glm::radians(rot.z);
-
-             // build rotation: yaw (Y) then pitch (X) then roll (Z)
-             glm::mat4 rot(1.0f);
-             rot = glm::rotate(rot, yaw,  glm::vec3(0.0f, 1.0f, 0.0f));
-             rot = glm::rotate(rot, pitch,glm::vec3(1.0f, 0.0f, 0.0f));
-             rot = glm::rotate(rot, roll, glm::vec3(0.0f, 0.0f, 1.0f));
-
-             // camera-space basis vectors (camera looks along -Z)
-             glm::vec3 right   = glm::normalize(glm::vec3(rot * glm::vec4(1,0,0,0)));
-             glm::vec3 up      = glm::normalize(glm::vec3(rot * glm::vec4(0,1,0,0)));
-             glm::vec3 forward = glm::normalize(glm::vec3(rot * glm::vec4(0,0,-1,0))); // -Z
-
-             shader.SetParameter(frameIndex, "camera.position", -forward * distance);
              shader.SetParameter(frameIndex, "camera.vFOVRadians", glm::radians(60.f));
-             shader.SetParameter(frameIndex, "camera.aspectRatio", 800.f/600.f);
+             shader.SetParameter(frameIndex, "camera.aspectRatio", WIDTH / (float)HEIGHT);
+             shader.SetParameter(frameIndex, "camera.angle", glm::radians(orbitAngle));
+             shader.SetParameter(frameIndex, "camera.distance", distance);
+
              shader.SetParameter(frameIndex, "bh.M", 1.f);
-             shader.SetParameter(frameIndex, "screenSize", glm::vec2(800, 600));
+
+             shader.SetParameter(frameIndex, "simParams.numSteps", simNumSteps);
+             shader.SetParameter(frameIndex, "simParams.stepSize", simStepSize);
+
+             shader.SetParameter(frameIndex, "screenSize", glm::vec2(WIDTH, HEIGHT));
              shader.SetParameter(frameIndex, "OutputTexture", &frameBuffer);
              shader.SetParameter(frameIndex, "Background", &background);
              p.Bind(cb, frameIndex);
 
-             vkCmdDispatch(cb.GetCommandBuffer(), 800 / 16, 800 / 16, 1); });
+             vkCmdDispatch(cb.GetCommandBuffer(), (WIDTH + 15) / 16, (HEIGHT + 15) / 16, 1); });
     app.Run();
     std::println("Hello world!");
 }
